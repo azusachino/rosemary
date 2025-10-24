6323039178b48aef9fb6a7efd96a684beac6a1c2
haru <azusachino@proton.me>
2025-10-24 14:40:08 +0900

    feat: async final sample

--- src/tests/async_basics.rs
+++ src/tests/async_basics.rs
@@ -1,11 +1,19 @@
 #[cfg(test)]
 mod tests_vol1 {
+    use anyhow::Result;
+    use futures_util::future::join_all;
     use std::{
+        fs::{File, OpenOptions},
+        io::Write,
         sync::{Arc, Mutex},
         task::{Poll, Waker},
         time::Duration,
     };
-    use tokio::{sync::mpsc, task, time::*};
+    use tokio::{
+        sync::mpsc,
+        task::{self, JoinHandle},
+        time::*,
+    };
 
     async fn slow_task() -> &'static str {
         sleep(Duration::from_secs(10)).await;
@@ -89,5 +97,72 @@ mod tests_vol1 {
         let outcome = task_handler.await.unwrap();
         println!("task completed with outcome: {}", outcome);
         trigger_task.await.unwrap();
+    }
+
+    type AsyncFileHandle = Arc<Mutex<File>>;
+    type FileJoinHandle = JoinHandle<Result<bool>>;
+
+    struct AsyncWriteFuture {
+        pub handle: AsyncFileHandle,
+        pub entry: String,
+    }
+
+    impl Future for AsyncWriteFuture {
+        type Output = Result<bool>;
+
+        fn poll(
+            self: std::pin::Pin<&mut Self>,
+            cx: &mut std::task::Context<'_>,
+        ) -> Poll<Self::Output> {
+            let mut guard = match self.handle.try_lock() {
+                Ok(guard) => guard,
+                Err(error) => {
+                    println!("error for {}: {}", self.entry, error);
+                    cx.waker().wake_by_ref();
+                    return Poll::Pending;
+                }
+            };
+            let lined_entry = format!("{}\n", self.entry);
+
+            match guard.write_all(lined_entry.as_bytes()) {
+                Ok(_) => println!("written for: {}", self.entry),
+                Err(e) => println!("{}", e),
+            };
+            Poll::Ready(Ok(true))
+        }
+    }
+
+    fn get_handle(file_path: &dyn ToString) -> AsyncFileHandle {
+        match OpenOptions::new().append(true).open(file_path.to_string()) {
+            Ok(file) => Arc::new(Mutex::new(file)),
+            Err(_) => Arc::new(Mutex::new(File::create(file_path.to_string()).unwrap())),
+        }
+    }
+
+    fn write_log(file_handle: AsyncFileHandle, line: String) -> FileJoinHandle {
+        let future = AsyncWriteFuture {
+            handle: file_handle,
+            entry: line,
+        };
+        task::spawn(async move { future.await })
+    }
+
+    #[tokio::test]
+    async fn test_3() {
+        let login_handle = get_handle(&"login.txt");
+        let logout_handle = get_handle(&"logout.txt");
+        let names = ["one", "two", "three", "four", "five"];
+        let mut handles = Vec::new();
+        for name in names {
+            let login = login_handle.clone();
+            let logout = logout_handle.clone();
+            let handle = write_log(login, name.to_owned());
+            let handle_2 = write_log(logout, name.to_owned());
+
+            handles.push(handle);
+            handles.push(handle_2);
+        }
+
+        let _ = join_all(handles).await;
     }
 }
